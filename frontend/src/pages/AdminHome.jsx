5f8063cb59d400eb9512298944f7f04a9ff1c1f0
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2023-09-24 16:51:06 +0900

    💄 Implement mobile GNB component

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -7,43 +7,55 @@ export default function AdminHome() {
       <div className="container">
         <div className="row">
           <div className="col-sm-4">
-            <div className="gnb-left">
-              <h1 className="logo">
-                <a href="/">
-                  <img src={logo} alt="Management" />
-                </a>
-              </h1>
+            <div className="gnb-wrapper">
+              <div className="gnb-left">
+                <h1 className="logo">
+                  <a href="/">
+                    <img src={logo} alt="Management" />
+                  </a>
+                </h1>
 
-              <nav className="gnb-nav lg-only">
-                <h2 className="visually-hidden">메뉴</h2>
-                <ul className="gnb-nav-list">
-                  <li className="gnb-nav-item">
-                    <a href="/">재고관리</a>
-                  </li>
-                  <li className="gnb-nav-item">
-                    <a href="/">출장일자</a>
-                  </li>
-                  <li className="gnb-nav-item">
-                    <a href="/">설정</a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-
-            <div className="gnb-right">
-              <div className="input-group lg-only">
-                <i className="ic-search" aria-hidden></i>
-                <input className="form-input" type="text" placeholder="검색" />
+                <nav className="gnb-nav lg-only">
+                  <h2 className="visually-hidden">메뉴</h2>
+                  <ul className="gnb-nav-list">
+                    <li className="gnb-nav-item">
+                      <a href="/">재고관리</a>
+                    </li>
+                    <li className="gnb-nav-item">
+                      <a href="/">비품목록</a>
+                    </li>
+                    <li className="gnb-nav-item">
+                      <a href="/">출장일자</a>
+                    </li>
+                    <li className="gnb-nav-item">
+                      <a href="/">설정</a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
 
-              <div className="button-group">
-                <button className="gnb-icon-button sm-only" type="button">
-                  <i className="ic-search"></i>
-                </button>
+              <div className="gnb-right">
+                <div className="input-group lg-only">
+                  <i className="ic-search" aria-hidden></i>
+                  <input
+                    className="form-input"
+                    type="text"
+                    placeholder="검색"
+                  />
+                </div>
 
-                <button className="gnb-avatar-button" type="button">
-                  <i className="ic-user" />
-                </button>
+                <div className="button-group">
+                  <button
+                    className="gnb-search-button gnb-icon-button sm-only"
+                    type="button"
+                  >
+                    <i className="ic-search"></i>
+                  </button>
+
+                  <button className="gnb-icon-button" type="button">
+                    <i className="ic-user" />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
